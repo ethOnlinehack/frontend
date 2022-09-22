b971f6d74a63060ad391aba1db21857a6e21cdf4
ZiedChekir <ziedchekir1@gmail.com>
2022-09-22 20:36:35 +0100

    create game page and routes

---
+++ pages/dashboard/game/[gameId]/nft/create.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateNft = () => {
+    return (
+        <div>
+            <h1>create nft page </h1>
+        </div>
+    );
+};
+
+export default CreateNft;
---
+++ pages/dashboard/game/[gameId]/nft/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nfts = () => {
+    return (
+        <div>
+            game nfts
+        </div>
+    );
+};
+
+export default Nfts;
