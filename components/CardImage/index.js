a2136b289d94c7d6adc13d5dc2fa79a21ae8fc3b
alaasouabni <alaa.souabni@supcom.tn>
2022-09-22 04:47:32 +0100

    added dropdown menu

--- components/CardImage/index.js
+++ components/CardImage/index.js
@@ -1,7 +1,9 @@
 import { Card } from "antd";
 import React from "react";
 const { Meta } = Card;
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { EditOutlined, DeleteOutlined, EllipsisOutlined, MenuOutlined   } from '@ant-design/icons';
+import { Menu } from 'antd';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
 import css from "./cardImage.module.scss"
 const CardImage = () => (
   <Card
@@ -16,13 +18,26 @@ const CardImage = () => (
       <DeleteOutlined  style={{color:"white"}}  key="ellipsis" />,
     ]}
     cover={
+      
       <img
         alt="example"
         src="./Cover.png"
       />
+      
     }
   >
+        <Menu mode="horizontal" style={{opacity:"20%", width:"64px"}}>
+    <Menu.SubMenu key="SubMenu" icon={<MenuOutlined />} >
+      <Menu.Item key="two" icon={<EditOutlined  />}>
+        Edit
+      </Menu.Item>
+      <Menu.Item key="three" icon={<DeleteOutlined />}>
+        Delete
+      </Menu.Item>
+      </Menu.SubMenu>
+    </Menu>
     <Meta style={{height:"20px", color:"white", fontSize:"50px",textAlign:"center"}}  title="RobinMania" />
+
   </Card>
 );
 
