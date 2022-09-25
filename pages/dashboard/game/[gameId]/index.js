40713115909a036c92ba09cca1c1a0a099feabcb
alaasouabni <alaa.souabni@supcom.tn>
2022-09-25 05:40:00 +0100

    access token

--- pages/dashboard/game/[gameId]/index.js
+++ pages/dashboard/game/[gameId]/index.js
@@ -7,6 +7,10 @@ import {Image } from "antd";
 import { useEffect } from "react";
 import { useAuth } from "../../../../contexts/Auth";
 import Router, { useRouter } from "next/router";
+import css from "./tab.module.scss";
+import { getOneGame } from "../../../../services/gameService";
+import { CopyOutlined } from "@ant-design/icons";
+import ButtonComponent from "../../../../components/Button";
 
 
 
@@ -15,9 +19,16 @@ const Game = () => {
   const router = useRouter();
   const { gameId } = router.query;
   const { isAuthenticated } = useAuth();
+  const [game, setGame] = useState([]);
   useEffect(() => {
     if (!isAuthenticated && isAuthenticated != null) Router.push("/login");
-  }, [gameId, router.isReady, isAuthenticated]);
+    if(router.isReady){
+    getOneGame({gameId:gameId}).then((data) => {
+      setGame(data);
+      console.log(data)
+;    });
+    }
+  }, [router.isReady]);
 
   const items = [
     {
@@ -36,8 +47,8 @@ const Game = () => {
   {router.isReady ? (
     <div>
       <div style={{ textAlign: "center" }}>
-        <h1 style={{position: "absolute", zIndex: "2", color:"white", marginTop:"125px", display:"inline-block", fontSize:"36px"  }}>
-          Game Name
+        <h1 style={{position: "absolute", zIndex: "2", color:"white", marginTop:"125px", display:"inline-block", fontSize:"36px", marginRight:"800px"  }}>
+          {game.name}
         </h1>
       </div>
       <Image
@@ -48,7 +59,15 @@ const Game = () => {
          height="300px"
          preview={false}
       />
-      <Tabs type="card" items={items} style={{marginTop:"50px"}} />
+      <div style={{display:"flex", alignItems:"center", justifyContent:"center", marginTop:"60px"}}>
+        <h1 style={{color:"white", fontSize:"18px", margin:"0", marginRight:"25px"}}>Access Token:</h1>
+      <ButtonComponent 
+  onClick={() =>  navigator.clipboard.writeText(game.access_token)}
+>
+<h1 style={{fontSize:"20px", color:"white", margin:"0", marginRight:"15px"}}>{game.access_token}</h1><CopyOutlined style={{fontSize:"16px", fontWeight:"bold"}} />
+</ButtonComponent>
+</div>
+      <Tabs className={css.tab} type="card" items={items} style={{marginTop:"50px"}} />
       </div>) : (
         " ")}
       
