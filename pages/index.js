fa00838e15a2a5e3d8953db8a08046991d3c9bf8
alaasouabni <alaa.souabni@supcom.tn>
2022-09-25 03:04:34 +0100

    hme

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,63 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.scss";
-import SignupForm from "../components/Form/index";
-import LoginForm from "../components/loginForm";
+import { Image } from "antd";
 import { useEffect } from "react";
 import { useAuth } from "../contexts/Auth";
 import Link from "next/link";
 import ButtonComponent from "../components/Button";
-import FileUpload from "../components/Upload";
 import { API } from "../services/routes";
-
 import CardImage from "../components/CardImage";
-
 import SimpleCard from "../components/SimpleCard";
-import Navbar from "../components/Navbar/index"
+import { Col, Row } from "antd";
+import { BulbOutlined } from "@ant-design/icons";
 
 
 export default function Home() {
   const { user, isAuthenticated } = useAuth();
-  const handleChange =(info)=>{
-   
-      if (info.file.status !== 'uploading') {
-        console.log(info.file, info.fileList);
-      }
-  
-      if (info.file.status === 'done') {
-        console.log(`${info.file.name} file uploaded successfully`);
-      } else if (info.file.status === 'error') {
-        console.error(`${info.file.name} file upload failed.`);
-      }
-    
-  }
+
   return (
     <div>
-    <div style={{height:"2000px"}} className={styles.container}>
-    <ButtonComponent>HIII</ButtonComponent>
-   <FileUpload withCredentials={true} url={API.FILE_UPLOAD.URL} maxCount={1} onChange={handleChange}/>
-        <ButtonComponent onClick={()=>console.log("hiiii")} loading={false}>zied </ButtonComponent>
-        <Link href="/test">
-        <button>test </button>
-      </Link>
-      {user && user.email}
-      <LoginForm />
-      <div style={{display: "flex",
-    alignItems: "center",
-    justifyContent: "center"}}>
-      <SimpleCard image="./Cover.png" >
-        <SignupForm/>
-      </SimpleCard>
+      <div
+        style={{
+          textAlign: "center",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1
+          style={{
+            position: "absolute",
+            zIndex: "2",
+            color: "white",
+            marginTop: "500px",
+            display: "inline-block",
+            fontSize: "36px",
+          }}
+        >
+          Web3 Games Made Easy
+        </h1>
       </div>
-    </div>
+      <Image
+        src="/testCover.jpg"
+        style={{ opacity: "0.3", zIndex: "1", objectFit: "cover" }}
+        alt="example"
+        width="100%"
+        height="500px"
+        preview={false}
+      />
+      <Row gutter={24} >
+        <Col sm={24} md={8} style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+          <SimpleCard style={{width:"400px", display:"flex", alignItems:"left", justifyContent:"left", padding:"20px", borderColor:"#474c53"}}>
+            <BulbOutlined style={{margin:"10px"}} />
+            <p style={{display:"block"}}>Lorem Ipsum</p>
+            </SimpleCard>
+        </Col>
+        <Col sm={24} md={8} style={{display:"flex", alignItems:"center", justifyContent:"center", }}>
+          <SimpleCard style={{width:"400px", borderColor:"#474c53"}}></SimpleCard>
+        </Col>
+        <Col sm={24} md={8} style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+          <SimpleCard style={{width:"400px", borderColor:"#474c53"}}></SimpleCard>
+        </Col>
+      </Row>
     </div>
   );
 }
